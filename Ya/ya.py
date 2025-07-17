0f781a06a99f3c3f52b370fe79d55967c78b030e
AlbertZam354 <abricos235@gmail.com>
2025-07-17 18:36:08 +0500

    lesson7_added

---
+++ Ya/ya.py
@@ -0,0 +1,14 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://ya.ru")
+
+driver.get("https://vk.com")
+
+driver.back()
+
+sleep(5)
